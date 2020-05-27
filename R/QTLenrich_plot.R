271cd56f625e95f0364769042b155ffabfb6a1e2
pablobio <pablofonseca.bio@gmail.com>
2020-05-27 16:30:59 -0400

    commit update QTLmarkers update

--- R/QTLenrich_plot.R
+++ R/QTLenrich_plot.R
@@ -1,6 +1,6 @@
 #' Plot enrichment results for QTL enrichment analysis
 #'
-#' Takes the output from overlapping_amoung_groups function and creates a heatmap with the overlapping between groups
+#' Takes the output from qtl_enrich function and creates a bubble plot with enrichment results
 #' @param qtl_enrich The output from qtl_enrich function
 #' @param x Id column to be used from the qtl_enrich output
 #' @param pval P-value to be used in the plot. If "p_value" informed, a non-adjusted pvalue will be plot. If "padj" informed, the adjusted p-value from the qtl enrichment analysis will be plotted.
